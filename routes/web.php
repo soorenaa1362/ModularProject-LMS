a27b07ce3484a60ff10b17db94288ad8d32c8ccc
soorenaa1362 <soorenaa1362@gmail.com>
2022-09-04 11:17:54 +0430

    01.2 Register-Login-ResetPassword blade complete

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::get('/register', function(){
+    return view('auth.register');
+});
+
+Route::get('/login', function(){
+    return view('auth.login');
+});
